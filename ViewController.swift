7834ecfa66403c3467a5bb7e79b32c3ed30891aa
Reneen Allan <iamreneenallan@gmail.com>
2019-07-12 09:26:38 -0500

    Middle Iteration 1

--- ViewController.swift
+++ ViewController.swift
@@ -18,28 +18,53 @@ class ViewController: UIViewController {
     @IBAction func sunny(_ sender: UIButton) {
         let alertController = UIAlertController (title: "Sunny", message: "Whip out your sunglasses and sandals, but dont forgett= your SPF!",preferredStyle: <#T##UIAlertController.Style#>)
         
-        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         }
     }
     
     
     @IBAction func cloudy(_ sender: UIButton) {
+        
+        let alertController = UIAlertController (title: "Sunny", message: "Whip out your sunglasses and sandals, but dont forgett= your SPF!",preferredStyle: <#T##UIAlertController.Style#>)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+}
     }
     
     
     @IBAction func Rainy(_ sender: UIButton) {
+        
+        let alertController = UIAlertController (title: "Sunny", message: "Whip out your sunglasses and sandals, but dont forgett= your SPF!",preferredStyle: <#T##UIAlertController.Style#>)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+}
     }
     
     
     @IBAction func thunder(_ sender: UIButton) {
+        
+        let alertController = UIAlertController (title: "Sunny", message: "Whip out your sunglasses and sandals, but dont forgett= your SPF!",preferredStyle: <#T##UIAlertController.Style#>)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+}
     }
     
     
     @IBAction func windy(_ sender: Any) {
+        
+        let alertController = UIAlertController (title: "Sunny", message: "Whip out your sunglasses and sandals, but dont forgett= your SPF!",preferredStyle: <#T##UIAlertController.Style#>)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+}
     }
     
     
     @IBAction func snow(_ sender: Any) {
+        
+        let alertController = UIAlertController (title: "Sunny", message: "Whip out your sunglasses and sandals, but dont forgett= your SPF!",preferredStyle: <#T##UIAlertController.Style#>)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+}
     }
     
     
